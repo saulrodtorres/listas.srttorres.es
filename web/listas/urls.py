6073a47cd1b2bf82d2b8e122f1504a95b26276ae
saulrodtorres <saul.rodriguez@upm.es>
2024-05-13 19:31:22 +0200

    Activating models in Django and doing migrations

--- web/listas/urls.py
+++ web/listas/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import vista_home, vista_tareas, vista_listas
+from .views import vista_home, vista_tarea, vista_listas
 
 urlpatterns = [
     path("", vista_listas, name="home"), # http://127.0.0.1:8000/listas/
     path("home/", vista_home, name="home"),
-    path("tareas/", vista_tareas, name="tareas"),
-    path("listas/", vista_tareas, name="listas"),
+    path("tarea/", vista_tarea, name="tarea"),
+    path("l/", vista_tarea, name="listas"), #aquí va el ID de la lista
 ]
--- web/listas/views.py
+++ web/listas/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Tarea
 # Create your views here.
 def vista_home(request):
     return HttpResponse('Hola, soy la vista home para home/ y para /')
 
-def vista_tareas(request):
-    return HttpResponse('Hola, soy la vista tareas')
+def vista_tarea(request):
+    tarea = Tarea()
+    tarea.descripcion = 'Tarea de prueba'
+    tarea.estado = 'TODO'
+    tarea.author_id = 1001
+    tarea.save()
+    pagina = "<html><body><h1>La tarea:</h1> <h2>%s</h2></body></html>" % tarea
+    http_response = HttpResponse(pagina)
+    return http_response
 
 def vista_listas(request):
     return HttpResponse('Hola, soy la vista listas')
