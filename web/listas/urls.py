6bc8c94dc7aaf7112d5e19079b75d82d2aa450ad
saulrodtorres <saul.rodriguez@upm.es>
2024-05-19 02:38:53 +0200

    comentarios y reestructurado de views. Ahora toca leer el tutorial de Django

--- web/listas/urls.py
+++ web/listas/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", vista_home, name="home"), # http://127.0.0.1:8000/ mostrará la colección de listas de tareas
+    path("", vista_home, name="home"),                                      # http://127.0.0.1:8000/ mostrará la colección de listas de tareas
     path("home/", vista_home, name="home"),
-    path("tarea/<int:tarea_id>/", vista_tarea, name="tarea"),#habría que hacerla modificable
-    path("lista-tareas/lista1", vista_lista, name="lista"), #aquí va el ID de la lista
-    path("lista-tareas/", vista_lista, name="crear_lista"), #aquí se crea una lista con los valores por defecto
+    path("tarea/<str:nombre_autor>/<str:nombre_lista>/<str:nombre_tarea>/", vista_tarea, name="tarea"),               
+                                                                            # habría que hacerla modificable. creo que debería agruparse por autor
+    
+    path("lista-tareas/nueva", vista_nueva_lista, name="nueva_lista"),      # http://127.0.0.1:8000/lista-tareas/nueva aquí se crea una lista con los valores por defecto
+    path("lista-tareas/<str:nombre_lista>", vista_lista, name="lista"),     # aquí va el ID de la lista
+    
     
 ]
--- web/listas/views.py
+++ web/listas/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse
 
 from .models import *
-# Create your views here.
+
+
 def vista_home(request):
+    # Vista para la página principal
+    # TODO: leer sobre las colecciones de objetos en django
+
     return HttpResponse('En la vista home debería aparecer la colección de listas de tareas')
 
-def vista_tarea(request, tarea_id):
+def vista_tarea(request, nombre_autor, nombre_lista, nombre_tarea):
+    # Vista para "tarea/<str:nombre_autor>/<str:nombre_lista>/<str:nombre_tarea>/"
+    # TODO: tratamiento nombre_autor
+    # TODO: tratamiento nombre_lista
+    # TODO: tratamiento nombre_tarea
+
     try:
         tarea = Tarea.objects.get(id=tarea_id)
     except Tarea.DoesNotExist:
@@ -26,24 +35,44 @@ def vista_tarea(request, tarea_id):
     http_response = HttpResponse(pagina)
     return http_response
 
-def vista_crear_lista(request):
-    return HttpResponse('Hola, soy la vista crear lista')
+def vista_nueva_lista(request):
+    # Vista para "lista-tareas/nueva"
+    # TODO: El valor del nombre y el author id deben guardarse en el modelo
+    # TODO: Debería crearse una plantilla a partir de este formulario
 
-def vista_lista(request):
+    
+    pagina = """<html><body>
+        <form method="POST">
+        <label>Nombre de la Lista:</label>
+        <input type="text" name="descripcion" value="">
+        <label>ID de Autor</label>
+        <input type="text" name="author_id" value="">
+        <input type="submit" value="Guardar">
+
+        </form>
+
+        </body></html>
+        """
+    http_response = HttpResponse(pagina)
+    return http_response
+
+def vista_lista(request, nombre_lista): 
+    # Vista para "lista-tareas/<str:nombre_lista>"
+    # TODO: debería ser un slug pero de momento en urls es un str
     #esto debería recibir un ID de lista
     try:
-        lista = Lista("nombre aleatorio", "993882")
+        lista = Lista(nombre="nombre aleatorio", author_id="993882")
     except Tarea.DoesNotExist:
         raise Http404(f"Lo siento, No se ha creado la lista.")
     
-    pagina = """<html><body><h1>Lista de tareas de usuario %s:</h1>
+    pagina = """<html><body>
         <form method="POST">
-        <label>Descripción:</label>
-        <input type="text" name="descripcion" value="%s"><br>
-        <label>Estado:</label>
+        <label>Nombre de la Lista:</label>
+        <input type="text" name="descripcion" value="%s"><label>Author ID:%s</label>
         <input type="submit" value="Actualizar">
         </form>
+
         </body></html>
-        """ % (lista.author_id, lista.nombre)
+        """ % (lista.nombre, lista.author_id)
     http_response = HttpResponse(pagina)
     return http_response
