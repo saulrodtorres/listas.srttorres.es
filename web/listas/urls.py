e7dda0e0068587e599145d9a1c29173ee6a14cd8
saulrodtorres <saul.rodriguez@upm.es>
2024-05-27 00:35:30 +0200

    limpieza de comentarios

--- web/listas/urls.py
+++ web/listas/urls.py
@@ -5,8 +5,8 @@ app_name = "listas"
 urlpatterns = [
     path("", vista_home, name="home"),                                                                  # http://127.0.0.1:8000/ mostrará la colección de listas de tareas
     path("home/", vista_home, name="home"),
-    path("lista-tareas/", nueva_lista, name="nueva_lista"),                                  # http://127.0.0.1:8000/lista-tareas/nueva aquí se crea una lista con los valores por defecto    
-    path("lista-tareas/<int:lista_pk>", vista_lista, name="lista"),            # http://127.0.0.1:8000/saul/lista-tareas/1/tarea/1/ El autor no haría falta, bastaría con el PK
+    path("lista-tareas/", nueva_lista, name="nueva_lista"),                    # http://127.0.0.1:8000/lista-tareas aquí se crea una lista de tareas
+    path("lista-tareas/<int:lista_pk>", vista_lista, name="lista"),            # http://127.0.0.1:8000/lista-tareas/70 mostrar la lista de tareas con ID = 70
     path("lista-tareas/<int:lista_pk>/tarea/<int:tarea_pk>/", vista_tarea, name="tarea"),               
                                                                                                         # habría que hacerla modificable. creo que debería agruparse por autor
     #path('', include(('listas.urls', 'listas'), namespace='listas')), fixed by app_name
