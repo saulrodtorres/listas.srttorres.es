5a216334935e8133dd1db34252899f7add0f89b9
saulrodtorres <saul.rodriguez@upm.es>
2024-05-27 00:28:08 +0200

    lista-tareas/ va al formulario pero no recoge bien los valores

--- web/listas/urls.py
+++ web/listas/urls.py
@@ -1,19 +1,17 @@
-from django.urls import path
-
+from django.urls import include, path
+from . import views
 from .views import *
-
+app_name = "listas"
 urlpatterns = [
-
     path("", vista_home, name="home"),                                                                  # http://127.0.0.1:8000/ mostrará la colección de listas de tareas
     path("home/", vista_home, name="home"),
-    path("<slug:slug_nombre_autor>/lista-tareas/<int:lista_pk>", vista_lista, name="lista"),            # http://127.0.0.1:8000/saul/lista-tareas/1/tarea/1/ El autor no haría falta, bastaría con el PK
-    path("<slug:slug_nombre_autor>/lista-tareas/<int:lista_pk>/tarea/<int:tarea_pk>/", vista_tarea, name="tarea"),               
+    path("lista-tareas/", nueva_lista, name="nueva_lista"),                                  # http://127.0.0.1:8000/lista-tareas/nueva aquí se crea una lista con los valores por defecto    
+    path("lista-tareas/<int:lista_pk>", vista_lista, name="lista"),            # http://127.0.0.1:8000/saul/lista-tareas/1/tarea/1/ El autor no haría falta, bastaría con el PK
+    path("lista-tareas/<int:lista_pk>/tarea/<int:tarea_pk>/", vista_tarea, name="tarea"),               
                                                                                                         # habría que hacerla modificable. creo que debería agruparse por autor
-    path("lista-tareas/nueva", vista_nueva_lista, name="nueva_lista"),                                  # http://127.0.0.1:8000/lista-tareas/nueva aquí se crea una lista con los valores por defecto
-    path("<slug:slug_nombre_autor>/lista-tareas/<int:lista_pk>", update_nueva_lista, name="update_nueva_lista"),
+    #path('', include(('listas.urls', 'listas'), namespace='listas')), fixed by app_name
+    
     # borrar posicion = borrar tarea con ID = PK
     # Completar tarea por posicion = Completar tarea con ID = PK
-    
-    
-    
 ]
+
--- web/listas/views.py
+++ web/listas/views.py
@@ -16,7 +16,7 @@ def vista_home(request):
 
     return HttpResponse('En la vista home debería aparecer la colección de listas de tareas')
 
-def vista_tarea(request, slug_nombre_autor, lista_pk, tarea_pk):
+def vista_tarea(request, lista_pk, tarea_pk):
     # Vista para "<str:slug_nombre_autor>/lista-tareas/<int:slug_nombre_lista>/tarea/<str:slug_nombre_tarea>"
     # Es un formulario para editar una tarea.
     try:
@@ -25,22 +25,17 @@ def vista_tarea(request, slug_nombre_autor, lista_pk, tarea_pk):
         raise Http404(f"Lo siento, pero la lista con PK {tarea_pk} no existe. Revisa si el PK de la tarea es correcta")    
     context = {                
         "tarea": tarea_actual,                          #Esto es un objeto Tarea. Solo hace falta decir tarea_pk porque eso la hace única.
-    }    
+    }
+    #podría comprobarse que la tarea_pk está en la lista_pk   
     return render(request, "listas/tarea.html", context=context)
 
-def vista_nueva_lista(request):
-    # Vista para "lista-tareas/nueva"
-    # Aquí se crea una lista con los valores vacíos para que en el formulario se asignen.
-    lista = Lista()
-    lista.nombre = 'Play Adrifu'
-    context = {
-        "lista_id" : lista.pk
-    }
-    return render(request, "listas/nueva_lista.html", context=context)
-
-def update_nueva_lista(request, slug_nombre_autor, lista_id):
+def nueva_lista(request):
     # Vista para guardar la nueva lista
-    nueva_lista = get_object_or_404(Lista, pk=lista_id)
+    lista = Lista()
+    lista_id = lista.pk
+    lista.nombre = "La lista del Caesar"
+    nueva_lista = get_object_or_404(Lista, pk=1)
+    print(nueva_lista)
     try:
         nueva_lista = Lista.objects.get(pk=lista_id)
     except (KeyError, Lista.DoesNotExist):
@@ -49,25 +44,24 @@ def update_nueva_lista(request, slug_nombre_autor, lista_id):
             'error_message': "No se ha podido cargar la lista",
         })
     else:
-        nueva_lista.nombre = request.POST['nombre']
-        nueva_lista.author_id = slug_nombre_autor #esto no tiene mucho sentido, solo cuando no cambia el slug y el autor
+        nueva_lista.nombre = request.POST['nombre_lista'] #recojo el name del input
+        nueva_lista.author_id = request.POST['autor_lista'] #esto no tiene mucho sentido, solo cuando no cambia el slug y el autor
         nueva_lista.slug_nombre = slugify(nueva_lista.nombre)
         nueva_lista.slug_author_id = slugify(nueva_lista.author_id)
         nueva_lista.save()
-        return HttpResponseRedirect(reverse('listas:lista', args=(nueva_lista.slug_author_id, nueva_lista.pk)))
+        return HttpResponseRedirect(reverse('listas:lista', args=(nueva_lista.pk)))
 
 
-def vista_lista(request, slug_nombre_autor, lista_pk): 
+def vista_lista(request, lista_pk): 
     # Vista para "<str:nombre_autor>/lista-tareas/<int:lista_pk>"
     # Es un formulario para editar el nombre de una lista y añadir tareas a la lista    
     try:
         lista_actual = Lista.get_lista_por_pk(pk=lista_pk)
     except Lista.DoesNotExist:
-        raise Http404(f"Lo siento, pero la lista con PK {lista_pk} no existe para el autor {slug_nombre_autor} pedido. Revisa si el slug del autor es correcto")    
+        raise Http404(f"Lo siento, pero la lista con PK {lista_pk}")    
     coleccion_tareas = Tarea.objects.filter(lista_id=lista_actual.pk)#esto está mal, debería ser por lista_id
     context = {        
         "nombre_lista_actual" : lista_actual.nombre,    #Se usa en el Título
-        "slug_nombre_autor": slug_nombre_autor,         #Se usa en el Subtítulo
         "coleccion_tareas": coleccion_tareas,           #TODO: esto es un <QuerySet [<Tarea: Descripción: Recoger la mesa de mi habitación (TODO). Author: saul>, <Tarea: Descripción: Recoger la ropa tendida (TODO). Author: saul>]>                
         "lista_actual_pk"   : lista_actual.pk
     }    
