5bcf175448a42ce448809ac5b9e123642c7fb316
saulrodtorres <saul.rodriguez@upm.es>
2024-05-27 18:38:55 +0200

    añadida funcionalidad borrar

--- web/listas/urls.py
+++ web/listas/urls.py
@@ -4,10 +4,9 @@ from .views import *
 app_name = "listas"
 urlpatterns = [
     path("", vista_home, name="home"),                                                                  # http://127.0.0.1:8000/ mostrará la colección de listas de tareas
-    path("home/", vista_home, name="home"),
     path("lista-tareas/", views.nueva_lista, name="nueva_lista"),                    # http://127.0.0.1:8000/lista-tareas aquí se crea una lista de tareas
     path("lista-tareas/<int:lista_pk>", views.vista_lista, name="lista"),            # http://127.0.0.1:8000/lista-tareas/70 mostrar la lista de tareas con ID = 70
-    path("lista-tareas/<int:lista_pk>", views.borrar_lista, name="borrar_lista"),            # http://127.0.0.1:8000/lista-tareas/70 mostrar la lista de tareas con ID = 70
+    path("lista-tareas/<int:lista_pk>/borrar", views.borrar_lista, name="borrar_lista"),            # http://127.0.0.1:8000/lista-tareas/70 mostrar la lista de tareas con ID = 70
     path("lista-tareas/<int:lista_pk>/tarea/<int:tarea_pk>/", vista_tarea, name="tarea"),               
                                                                                                         # habría que hacerla modificable. creo que debería agruparse por autor
     #path('', include(('listas.urls', 'listas'), namespace='listas')), fixed by app_name
